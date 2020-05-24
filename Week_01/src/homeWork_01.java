96f3885a6d1dc659cf314e57c8f281b9b32c0474
lucienhsu <happyfun365@126.com>
2020-05-24 21:09:09 +0800

    [add] 作业提交

---
+++ Week_01/src/homeWork_01.java
@@ -0,0 +1,34 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 用 add first 或 add last 这套新的 API 改写 Deque 的代码
+ */
+public class homeWork_01 {
+    /**
+     * @return
+     */
+    public void solution() {
+        Deque<String> deque = new LinkedList<>();
+
+        deque.addLast("a");
+        deque.addLast("b");
+        deque.addLast("c");
+        System.out.println(deque);
+
+        String str = deque.getFirst();
+        System.out.println(str);
+        System.out.println(deque);
+
+        while (deque.size()>0){
+            System.out.println(deque.removeFirst());
+        }
+        System.out.println(deque);
+    }
+
+
+    public static void main(String args[]) {
+        new homeWork_01().solution();
+    }
+
+}
